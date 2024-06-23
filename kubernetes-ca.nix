0fdde81ae312850db4b16c3d2a66be54395b7f7d
rydnr <github@acm-sl.org>
2024-06-23 18:48:25 +0200

    More tests

--- kubernetes-ca.nix
+++ kubernetes-ca.nix
@@ -11,7 +11,7 @@ let
   cfg = config.services.raw-kubernetes-ca;
   description = "Manages the certificate authority used by Kubernetes";
   generateCaCert = pkgs.writeScriptBin "generate-ca-cert" ''
-    #!/usr/bin/env bash
+    #!/usr/bin/env /bin/bash
     set -e
     CA_NAME="$1";
     DAYS="$2";
