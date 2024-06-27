f1fda0f017f708b0e89788a5325e4ca1867082fe
rydnr <github@acm-sl.org>
2024-06-27 21:54:21 +0200

    Another attempt

--- kube-proxy.nix
+++ kube-proxy.nix
@@ -10,7 +10,7 @@ with lib;
 
 let
   cfg = config.services.raw-kube-proxy;
-  originalKubernetes = import "${nixpkgs}/nixos/modules/services/cluster/kubernetes/default.nix";
+  originalKubernetes = import "${nixpkgs.path}/nixos/modules/services/cluster/kubernetes/default.nix";
   mkCert = originalKubernetes.mkCert;
   mkKubeConfig = originalKubernetes.mkKubeConfig;
   generatedKubeConfig = mkKubeConfig "raw-kube-proxy" cfg;
